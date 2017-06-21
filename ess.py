7c706fd3df964f84604b15912a19482b308725ba
avinash-sudhodanan <avinash.sudhodanan@gmail.com>
2017-06-21 09:51:07 +0200

    Update ess.py with Docker Getting started Example
    Updated the ess.py file with the contents of app.py from https://docs.docker.com/get-started/part2/#apppy

--- ess.py
+++ ess.py
@@ -1,30 +1,24 @@
-def is_palindrome_v1(s):
-    """ (str) -> bool
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
 
-    Return True if and only if s is a palindrome
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
-    >>> is_palindrome_v1('noon')
-    True
-    >>> is_palindrome_v1('racecar')
-    True
-    is_palindrome_v1('dented')
-    False
-    """
-    return reverse(s)==s
+app = Flask(__name__)
 
-def reverse(s):
-    """ (str) -> (str)
-    
-    Return a reverse version of s.
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    >>> reverse("foo")
-    oof
-    >>> reverse("o")
-    o
-    """
-    rev=""
-    #For each character in s, add that char to the beginning of rev.
-    for ch in s:
-        rev=ch+rev
-    return rev
-print(reverse("ElasTest"))
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=80)
