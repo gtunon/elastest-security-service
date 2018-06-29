1d7c27895b9cb951a8b6255f97c141d795aca7b0
avinash-sudhodanan <avinash-sudhodanan@gmail.com>
2018-06-29 12:02:44 +0100

    Updated tests

--- e2e-test/e2etest.py
+++ e2e-test/e2etest.py
@@ -24,7 +24,7 @@ def e2etests():
 	print("TORM URL is: "+tormurl)
 	#List of all the tests to be run. Append to this list the new tests
 	#tests=["test_load_torm_home_preloader(tormurl,driver)","test_load_torm_home_full(tormurl+\"/api/context/services/info\",driver)","test_service_launch(tormurl,driver)","test_create_new_project(tormurl+\"/api/project\",driver)","test_create_new_tjob(tormurl+\"/api/tjob\")","test_run_tjob(tormurl,driver)"]
-	tests=["test_load_torm_homepage(tormurl,driver)","test_service_launch(tormurl,driver)","test_zap_active_scan(tormurl,driver)"]
+	tests=["test_load_torm_homepage(tormurl,driver)","test_create_exec_tjob(tormurl,driver)"]
 	#setup Chrome WebDriver
 	options = webdriver.ChromeOptions()
 	options.add_argument('headless')
@@ -74,64 +74,42 @@ def test_load_torm_homepage(tormurl,driver):
 			return "failed"
 		return "success"
 
-
-#Launch ESS service
-def test_service_launch(tormurl,driver):
+def test_create_exec_tjob(tormurl,driver):
+		driver.get(tormurl+"/#/projects/1")
 		try:
-			driver.get(tormurl+"#/support-services")
-			time.sleep(30)
-			print("\ta. Loaded TSS page")
-			element = driver.find_element_by_class_name("mat-select-trigger")
+			element = driver.find_element(By.XPATH, '//*[@id="tJobs"]/div/table/tbody/tr/td[7]/div/span/button[2]/span/md-icon')
 			element.click()
-			print("\tb. Clicked TSS Options")
-			options = driver.find_elements_by_tag_name("md-option")
-			print(options)
-			for option in options:
-				if "ESS" in str(option.get_attribute('innerHTML')):
-					option.click()
-					print("\tc. Selected ESS from the list of TSSes")
-			element=driver.find_element_by_id("create_instance")
+			print("\ta. Hello World TJob Edit Button Clicked")
+
+			element = driver.find_element(By.ID, 'md-input-3')
+			element.clear()
+			print("\tb. Hello World TJob Results Path Cleared")
+
+			element = driver.find_element(By.ID, 'md-input-5')
+			element.clear()
+			print("\tc. Hello World TJob Docker Image Cleared")
+			element.send_keys("dockernash/test-tjob-ess")
+			print("\td. ESS Test TJob Docke Image Set")
+
+			element = driver.find_element(By.ID, 'commands')
+			element.clear()
+			print("\te. Hello World TJob Commands Cleared")
+			element.send_keys("python fteaching-tjob.py example")
+			print("\tf. ESS Test TJob Commands Set")
+
+			element = driver.find_element(By.XPATH, '//*[@id="menusideLeft"]/md-sidenav-container/div[4]/div/div/etm-tjob-form/md-card/md-card-content/form/td-expansion-panel[2]/div[2]/div/div[4]/div/md-checkbox/label/div')
 			element.click()
-			print("\td. Initiated ESS launch")
-			element = WebDriverWait(driver, 540).until(
-		        EC.visibility_of_element_located((By.ID, "view_service"))
-		    )
+			print("\tg. Set ESS as TSS for the Test Tjob")
+
+			element = driver.find_element(By.XPATH, '//*[@id="menusideLeft"]/md-sidenav-container/div[4]/div/div/etm-tjob-form/md-card/md-card-actions/button[1]/span')
 			element.click()
-			print("\te. ESS launch successful")
+			print("\th. Saved the Test Tjob")
+
+			element = driver.find_element(By.XPATH, '//*[@id="menusideLeft"]/md-sidenav-container/div[4]/div/div/app-tjob-manager/div[1]/div/md-card/md-card-content/div/span[4]/div/span/button[1]/span/md-icon')
+			element.click()
+			print("\ti. Launched the Test Tjob")
+
 		except:
-			print("Failed to click Test Support Services Tab because: "+str(sys.exc_info()[0]))
+			print("\tERROR:: Test to load TORM home page prealoader failed")
 			return "failed"
 		return "success"
-
-# Function to test the scanning feature
-def test_zap_active_scan(tormurl,driver):
-		try:
-			element = driver.find_element_by_xpath("/html/body/etm-app/etm-etm/td-layout-nav/div/div/td-layout-manage-list/md-sidenav-container/div[4]/div/div/esm-service-detail/div/div/md-card/md-card-content/div[1]/md-list/div[1]/md-list-item[1]/div/a")
-			driver.get(element.text)
-			print("\ta. ESS GUI loaded successfully")
-			element = driver.find_element_by_id("scan-url")
-			element.send_keys("http://example.com")
-			print("\tb. Entered example.com URL")
-			element = driver.find_element_by_id("start-scan")
-			element.click()
-			print("\tc. Started scanning example.com")
-			element = WebDriverWait(driver, 540).until(
-		        EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/ul/li[3]/div[2]/ul/ul/li[1]/div[1]/i"))
-		    )
-			print("\td. Scanned example.com and generated report")
-		except:
-			print("Scanning of http://example.com failed")
-			return "failed"
-		return "success"
-"""
-# Function to make REST API calls to create a project in TORM
-def test_create_new_project(tormapicreateprojecturl,driver):
-
-# Create a new tjob as part of a project
-def test_create_new_tjob(tormapicreatetjoburl,driver):
-
-# Function to test the running of the tjob
-def test_run_tjob(tormurl,driver):
-"""
-if __name__=="__main__":
-	e2etests()
