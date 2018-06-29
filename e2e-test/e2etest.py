89a2054232cb55eb7afa04903de5db3cc8749044
avinash-sudhodanan <avinash-sudhodanan@gmail.com>
2018-06-29 15:26:56 +0100

    Updated according to dev version

--- e2e-test/e2etest.py
+++ e2e-test/e2etest.py
@@ -27,7 +27,7 @@ def e2etests():
 	tests=["test_load_torm_homepage(tormurl,driver)","test_create_exec_tjob(tormurl,driver)"]
 	#setup Chrome WebDriver
 	options = webdriver.ChromeOptions()
-	options.add_argument('headless')
+	#options.add_argument('headless')
 	options.add_argument('--no-sandbox')
 	#driver = webdriver.Firefox() #for testing with GUI locally
 	driver = webdriver.Chrome(chrome_options=options)
@@ -48,13 +48,13 @@ def e2etests():
 			#Check if the last test executed successfully.
 			if status=="success":
 				testssuccess+=1
-				print("Status: Success")
+				print("Test Status: Success")
 			if status=="failed":
 				testsfailed+=1
-				print("Status: Failed")
+				print("Test Status: Failed")
 				#A failed test will prevent the execution of future tests. This behavior is debatable.
 				break
-	driver.close()
+	#driver.close()
 	print("##############")
 	print("_TESTS SUMMARY_")
 	print("TOTAL TESTS RAN: "+str(testsrun))
@@ -75,41 +75,93 @@ def test_load_torm_homepage(tormurl,driver):
 		return "success"
 
 def test_create_exec_tjob(tormurl,driver):
-		driver.get(tormurl+"/#/projects/1")
 		try:
-			element = driver.find_element(By.XPATH, '//*[@id="tJobs"]/div/table/tbody/tr/td[7]/div/span/button[2]/span/md-icon')
+			element = driver.find_element(By.XPATH, '/html/body/etm-app/etm-etm/td-layout-nav/div/div/td-layout-manage-list/md-sidenav-container/div[4]/div/div/app-tjob-execs-manager/etm-projects-manager/div/div/div/md-card/md-card-content/td-data-table/div/table/tbody/tr/td[2]/div')
+			element.click()
+			print("\ta. Hello World Project Clicked")
+		except:
+			print("\tERROR:: Hello World Project Click failed because ")
+			return "failed"
+
+		try:
+			element = driver.find_element(By.XPATH, '//*[@id="tJobs"]/div/table/tbody/tr/td[7]/div/button[2]/span/md-icon')
 			element.click()
 			print("\ta. Hello World TJob Edit Button Clicked")
-
+		except:
+			print("\tERROR:: Hello World TJob Edit Button Click failed because "+E)
+			return "failed"
+		time.sleep(10)
+		try:
 			element = driver.find_element(By.ID, 'md-input-3')
 			element.clear()
 			print("\tb. Hello World TJob Results Path Cleared")
+		except:
+			print("\tERROR:: Hello World TJob Results Path Clearing failed")
+			return "failed"
 
+		try:
 			element = driver.find_element(By.ID, 'md-input-5')
 			element.clear()
 			print("\tc. Hello World TJob Docker Image Cleared")
+		except:
+			print("\tERROR:: Hello World TJob Docker Image Clearing failed")
+			return "failed"
+
+		try:
 			element.send_keys("dockernash/test-tjob-ess")
 			print("\td. ESS Test TJob Docke Image Set")
+		except:
+			print("\tERROR:: ESS Test TJob Docke Image Setting failed")
+			return "failed"
 
+		try:
 			element = driver.find_element(By.ID, 'commands')
 			element.clear()
 			print("\te. Hello World TJob Commands Cleared")
+		except:
+			print("\tERROR:: Hello World TJob Commands Clearing failed")
+			return "failed"
+
+		try:
 			element.send_keys("python fteaching-tjob.py example")
 			print("\tf. ESS Test TJob Commands Set")
+		except:
+			print("\tERROR:: ESS Test TJob Commands Setting failed")
+			return "failed"
 
-			element = driver.find_element(By.XPATH, '//*[@id="menusideLeft"]/md-sidenav-container/div[4]/div/div/etm-tjob-form/md-card/md-card-content/form/td-expansion-panel[2]/div[2]/div/div[4]/div/md-checkbox/label/div')
+		try:
+			element = driver.find_element(By.XPATH, '/html/body/etm-app/etm-etm/td-layout-nav/div/div/td-layout-manage-list/md-sidenav-container/div[4]/div/div/etm-tjob-form/md-card/md-card-content/form/td-expansion-panel[2]/div[2]/div/div[3]/div/md-checkbox/label/div')
 			element.click()
 			print("\tg. Set ESS as TSS for the Test Tjob")
+		except:
+			print("\tERROR:: Setting ESS as TSS for the Test Tjob failed")
+			return "failed"
 
+		try:
 			element = driver.find_element(By.XPATH, '//*[@id="menusideLeft"]/md-sidenav-container/div[4]/div/div/etm-tjob-form/md-card/md-card-actions/button[1]/span')
 			element.click()
 			print("\th. Saved the Test Tjob")
-
-			element = driver.find_element(By.XPATH, '//*[@id="menusideLeft"]/md-sidenav-container/div[4]/div/div/app-tjob-manager/div[1]/div/md-card/md-card-content/div/span[4]/div/span/button[1]/span/md-icon')
+		except:
+			print("\tERROR:: Attempt to Saved the Test Tjob failed")
+			return "failed"
+		time.sleep(10)
+		try:
+			element = driver.find_element(By.XPATH, '/html/body/etm-app/etm-etm/td-layout-nav/div/div/td-layout-manage-list/md-sidenav-container/div[4]/div/div/etm-project-manager/div[2]/div[1]/etm-tjobs-manager/md-card/md-card-content/td-data-table/div/table/tbody/tr/td[7]/div/button[1]')
 			element.click()
 			print("\ti. Launched the Test Tjob")
-
 		except:
-			print("\tERROR:: Test to load TORM home page prealoader failed")
+			print("\tERROR:: Launching the Test Tjob failed")
 			return "failed"
+		time.sleep(10)
+		try:
+			element = driver.find_element(By.XPATH, '//*[@id="resultMsgText"]')
+			while(element.text!="Executing Test" or element.text!="Failed" or element.text!="Finish"):
+				print("\tWaiting for tjob execution to complete")
+		except:
+			print("\tERROR:: Could not fetch the status of tjob execution")
+			return "failed"
+
+
 		return "success"
+if __name__=="__main__":
+	e2etests()
